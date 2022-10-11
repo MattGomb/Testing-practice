3151153af2fb7061bf4015bb44c6c1bf62128393
Matyas Gombos <gombosmm@gmail.com>
2022-10-11 16:47:14 -0500

    Adjust reverse test

--- 2) reverseString/reverseString.test.js
+++ 2) reverseString/reverseString.test.js
@@ -2,6 +2,9 @@ const reverseString = require('./reverseString.js');
 
 describe('Test reverse function', () => {
   test('"should return the opposite order"', () => {
-    expect(reverseString('1234')).toBe('4321');
+    expect(reverseString('kayak')).toBe('kayak');
+  });
+  test('"should return the opposite order"', () => {
+    expect(reverseString('123')).toBe('321');
   });
 });
